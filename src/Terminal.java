d227e6ff8eec358149745902d02172ca948c55b7
Mark77645 <Marksamirf@gmail.com>
2021-11-08 12:33:57 +0200

    Parser

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,7 +1,12 @@
 public class Terminal {
     Parser parser;
 //Implement each command in a method, for example:
-    public String pwd(){...} public void cd(String[] args){...} // ... //This method will choose the suitable command method to be called
+    public void echo(String argument){
+        System.out.println(argument);
+    }
+    public String pwd(){...}
+    public void cd(String[] args){...} // ... //This method will choose the suitable command method to be called
     public void chooseCommandAction(){...}
+
     public static void main(String[] args){...}
 }

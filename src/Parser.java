4132cc8b79279e025c78150fc61555d7362ab642
Mark77645 <Marksamirf@gmail.com>
2021-11-08 13:17:50 +0200

    Parser

--- src/Parser.java
+++ src/Parser.java
@@ -1,8 +1,19 @@
 class Parser {
-    String commandName;
-    String[] args; //This method will divide the input into commandName and args
+    public String commandName;
+    public String[] args;
+    //This method will divide the input into commandName and args
     // where "input" is the string command entered by the user
-    public boolean parse(String input){...}
-    public String getCommandName(){...}
-    public String[] getArgs(){...}
+    public boolean parse(String input){
+        String [] arr;
+        arr=input.split("\\s");
+        commandName=arr[0].toString();
+        args=arr;
+        return true;
+    }
+    public String getCommandName(){
+        return commandName;
+    }
+    public String[] getArgs(){
+        return args;
+    }
 }
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,11 +1,14 @@
-public class Terminal {
+public class Terminal extends Parser {
     Parser parser;
 //Implement each command in a method, for example:
     public void echo(String argument){
         System.out.println(argument);
     }
-    public String pwd(){...}
-    public void cd(String[] args){...} // ... //This method will choose the suitable command method to be called
+    public String pwd(){
+        System.out.println();
+    }
+    public void cd(String[] args){...}
+    // ... //This method will choose the suitable command method to be called
     public void chooseCommandAction(){...}
 
     public static void main(String[] args){...}

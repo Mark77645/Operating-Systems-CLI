b211ccc4754a697d28e28d6dd9ebe585b4ca99aa
Mark77645 <Marksamirf@gmail.com>
2021-11-11 14:39:29 +0200

    made some functions

--- src/Parser.java
+++ src/Parser.java
@@ -1,19 +1,27 @@
+import java.nio.file.Paths;
+
 class Parser {
     public String commandName;
     public String[] args;
-    //This method will divide the input into commandName and args
+
+    // This method will divide the input into commandName and args
     // where "input" is the string command entered by the user
-    public boolean parse(String input){
-        String [] arr;
-        arr=input.split("\\s");
-        commandName=arr[0].toString();
-        args=arr;
+    public boolean parse(String input) {
+        String[] arr;
+        arr = input.split("\\s"); // Holding the first string which is the command itself
+        commandName = arr[0].toString();// Command name is now the first index in the array
+        // For loop that copies all the arguments of the command except for the command name (arr[0]).
+        for (int i = 0; i < arr.length; i++) { 
+            args[i] = arr[i + 1];
+        }
         return true;
     }
-    public String getCommandName(){
+
+    public String getCommandName() {
         return commandName;
     }
-    public String[] getArgs(){
+
+    public String[] getArgs() {
         return args;
     }
 }
--- src/Terminal.java
+++ src/Terminal.java
@@ -1,15 +1,56 @@
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Terminal extends Parser {
     Parser parser;
 //Implement each command in a method, for example:
     public void echo(String argument){
         System.out.println(argument);
     }
-    public String pwd(){
-        System.out.println();
+    public void pwd(){
+        System.out.println(getArgs());
     }
-    public void cd(String[] args){...}
+    public void cd(String[] args){
+
+    }
+    public void ls(){
+
+    }
+    public void touch(String args){
+        String [] file; //Array that holds all the paths that are separated by a "\".
+        String FileName=""; //Holds the last name in the path.
+        file=(args.split("\\"));//holds each path in a separate index.
+        for(int i=0;i<file.length;i++){
+            FileName=file[i].toString(); //To reach the final name.
+        }
+        File NewFile = new File(FileName);
+    }
+    public void cp(String args) throws IOException{
+        String [] files;
+        files=(args.split("\\"));
+        String FirstFile=files[0];
+        String SecondFile=files[1];
+        FileWriter Wfile=new FileWriter(SecondFile) ;
+        Scanner myReader = new Scanner(FirstFile);
+        while (myReader.hasNextLine()) {
+            String data = myReader.nextLine();
+            Wfile.write(data);
+      }
+        myReader.close();
+        Wfile.close();
+    }
     // ... //This method will choose the suitable command method to be called
     public void chooseCommandAction(){}
 
-    public static void main(String[] args){...}
+    public static void main(String[] args){
+        try{
+
+        }catch{
+
+        }
+    }
 }

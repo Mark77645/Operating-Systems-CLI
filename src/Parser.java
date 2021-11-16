5e229694088e57354a5a53e4eb9bcfaab5aa1f76
Mark77645 <Marksamirf@gmail.com>
2021-11-16 20:25:17 +0200

    Update Parser.java
    Mark's Final

--- src/Parser.java
+++ src/Parser.java
@@ -1,20 +1,24 @@
 class Parser {
-    public String commandName;
-    public String[] args;
+    String commandName;
+    String[] args;
+    Parser(){
+        args=null;
 
-    // This method will divide the input into commandName and args
-    // where "input" is the string command entered by the user
-    public boolean parse(String input) {
-        String[] arr;
-        arr = input.split("\\s"); // Holding the first string which is the command itself
-        commandName = arr[0].toString();// Command name is now the first index in the array
-        // For loop that copies all the arguments of the command except for the command name (arr[0]).
-        for (int i = 0; i < arr.length; i++) { 
-            args[i] = arr[i + 1];
-        }
-        return true;
     }
-
+    //This method will divide the input into commandName and args
+    // where "input" is the string command entered by the user
+    public boolean parse(String input){
+        String[] arr;
+        arr = input.split(" "); // Holding the first string which is the command itself
+        commandName = arr[0];// Command name is now the first index in the array
+        // For loop that copies all the arguments of the command except for the command name (arr[0]).
+        args=new String[arr.length-1];
+        for(int i = 1 ;i<arr.length;i++){
+            args[i-1]=arr[i];
+        }
+        //System.out.println(commandName+" "+args.toString());
+    return true;
+    }
     public String getCommandName() {
         return commandName;
     }

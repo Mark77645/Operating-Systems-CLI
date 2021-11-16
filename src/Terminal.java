6c79ed04e63c13268b682e9521c2ee86e384fb5e
Mark77645 <Marksamirf@gmail.com>
2021-11-16 20:23:39 +0200

    Update Terminal.java
    Mark's final

--- src/Terminal.java
+++ src/Terminal.java
@@ -1,84 +1,218 @@
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Terminal extends Parser {
-    Parser parser;
+    public Parser parser;
+
+    Terminal() {
+        parser = new Parser();
+    }
 
     //Implement each command in a method, for example:
-    public void echo(String argument) {
-        System.out.println(argument);
+    public void echo(String[] argument) {
+        for (int i = 0; i < argument.length; i++) {
+            System.out.print(argument[i]);
+            if (i == argument.length - 1) {
+                break;
+            }
+            System.out.print(" ");
+        }
+        System.out.print("\n");
     }
 
     public void pwd() {
-        System.out.println(getArgs());
+        Path currentDir = Paths.get("");
+        System.out.println(currentDir.toAbsolutePath().toString());
+        System.out.print("\n");
     }
 
     public void cd(String[] args) {
 
     }
 
-    public void ls() {
-
-    }
-
-    public void touch(String args) {
-        String[] file; //Array that holds all the paths that are separated by a "\".
-        String FileName = ""; //Holds the last name in the path.
-        file = (args.split("\\"));//holds each path in a separate index.
-        for (int i = 0; i < file.length; i++) {
-            FileName = file[i].toString(); //To reach the final name.
+    public void ls(File currList) {
+        File[] filesList = currList.listFiles();
+        for (File file : filesList) {
+            if (file.isDirectory())
+                System.out.print(file.getName() + "\t");
+            if (file.isFile()) {
+                System.out.println(file.getName() + "\t");
+            }
         }
-        File NewFile = new File(FileName);
     }
 
-    public void cp(String args) throws IOException {
-        String[] files;
-        files = (args.split("\\"));
-        String FirstFile = files[0];
-        String SecondFile = files[1];
-        FileWriter Wfile = new FileWriter(SecondFile);
-        Scanner myReader = new Scanner(FirstFile);
+    public void lsReverse(File currList) {
+        File[] filesList = currList.listFiles();
+        for (int i = filesList.length - 1; i >= 0; i--) {
+            if (filesList[i].isDirectory()) {
+                System.out.print(filesList[i].getName() + "\t");
+            }
+            if (filesList[i].isFile()) {
+                System.out.println(filesList[i].getName() + "\t");
+            }
+        }
+    }
+
+    public void touch(String [] args) throws IOException {
+        File NewFile = new File(args[0]);
+        NewFile.createNewFile();
+    }
+
+    public void cp(String [] args) throws IOException {
+        File readFile= new File(args[0]);
+        FileWriter writeFile = new FileWriter(args[1]);
+        Scanner myReader = new Scanner(readFile);
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
-            Wfile.write(data);
+            writeFile.write(data);
         }
         myReader.close();
-        Wfile.close();
+        writeFile.close();
     }
 
-    public void rm(String FileName) {
-        File file = new File(FileName);
+    public void rmdir(String[] args_,File curdir) {
+        String first = args_[0];
+
+        File[] file_ = curdir.listFiles();
+        if (first.contains("*")){
+            for (int i = 0; i < file_.length; i++) {
+                File file = new File(file_[i].getName());
+                if (file.exists() && file.isDirectory() && file.listFiles().length==0){
+                 file.delete();
+                    System.out.println("done");
+                }
+            }
+        }
+//        File file_ = new File(fileName[fileName.length-1]);
+//        for (int i = 0; i < fileName.length; i++) {
+//            if (file_.isAbsolute()) {
+//                for (int j = 0; j < fileName.length; j++) {
+//                    File file = new File(file_.getAbsolutePath());
+//                    boolean make_ = file.delete();
+//                }
+//            }
+//            boolean file = new File(fileName[i]).delete();
+//            if (i == fileName.length - 1) {
+//                break;
+//            }
+//        }
+
+//        if (fileName[0].equals("*") && file_.exists()) {
+//            for (File file : filesList) {
+//                if (file.length() == 0)
+//                    file.delete();
+//                if (file.isFile()) {
+//                    System.out.println(file.getName() + "\t");
+//                }
+//            }
+//        }
+
+    }
+
+
+    public void mkdir(String[] fileName) {
+        File file_ = new File(fileName[fileName.length-1]);
+        for (int i = 0; i < fileName.length; i++) {
+            if (file_.isAbsolute()) {
+                for (int j = 0; j < fileName.length; j++) {
+                    File file = new File(file_.getAbsolutePath());
+                    boolean make_ = file.mkdir();
+                }
+            }
+            boolean file = new File(fileName[i]).mkdir();
+            if (i == fileName.length - 1) {
+                break;
+            }
+        }
+    }
+
+    public void rm(String[] FileName) {
+        File file = new File(FileName[0]);
         if (file.exists()) {
             file.delete();
         } else {
-            System.out.println("Nothing to delete");
+            System.out.println(file.getAbsolutePath() + " No such file!");
         }
     }
 
-    public void cat(String file) throws IOException {
-        FileWriter Wfile = new FileWriter(file);
-        Scanner myReader = new Scanner(file);
+    public void cat(String [] file) throws IOException {
+        File file1 = new File(file[0]);
+        Scanner myReader = new Scanner(file1);
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
-            Wfile.write(data);
+            System.out.println(data);
         }
         myReader.close();
-        Wfile.close();
     }
 
     // ... //This method will choose the suitable command method to be called
-    public void chooseCommandAction() {
-
+    public void chooseCommandAction() throws IOException {
+        String thisCommand = this.parser.getCommandName();
+        File currFile = new File(".");
+        switch (thisCommand) {
+            case "echo":
+                echo(this.parser.getArgs());
+                break;
+            case "pwd":
+                pwd();
+                break;
+            case "cd":
+                break;
+            case "ls":
+                if (Arrays.toString(this.parser.getArgs()).contains("-r")) {
+                    lsReverse(currFile);
+                } else {
+                    ls(currFile);
+                }
+                break;
+            case "rm":
+                rm(this.parser.getArgs());
+                break;
+            case "touch":
+                touch(this.parser.getArgs());
+                break;
+            case "mkdir":
+                mkdir(this.parser.getArgs());
+                break;
+            case "rmdir":
+                rmdir(this.parser.getArgs(),currFile);
+                break;
+            case "cat":
+                cat(this.parser.getArgs());
+                break;
+            case "cp":
+                cp(this.parser.getArgs());
+                break;
+            default:
+                System.out.println("Error: Command not found or invalid parameters are entered!");
+        }
     }
 
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws IOException {
 
-        } catch() {
+//        File file=new File(".");
+//        System.out.println("Current Working Directory: " + file.getAbsolutePath());
+//        System.setProperty("user.dir", "/tmp");
+//        System.out.println("New Current Working Directory: " + file.getAbsolutePath());
+
+        Terminal terminal = new Terminal();
+        Scanner input = new Scanner(System.in);
+        String _command = new String();
+        boolean flag = true;
+        while (flag) {
+            System.out.print(">");
+            _command = input.nextLine();
+            if (_command.equals("exit")) {
+                flag = false;
+                break;
+            }
+            terminal.parser.parse(_command);
+            terminal.chooseCommandAction();
 
         }
     }
